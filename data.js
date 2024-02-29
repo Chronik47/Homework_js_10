c762217beec614293964cf36a163af92bbf762f2
Chronik <vladislamakarevich@mail.ru>
2024-02-29 17:57:34 +0300

    домашнее задание по js 10

---
+++ data.js
@@ -0,0 +1,22 @@
+const dataInfo = `
+[
+    {
+    "year": "1703",
+      "title": "Санкт-Петербург",
+      "url": "https://avatars.mds.yandex.net/get-altay/4341149/2a0000017b303b2bf816814eedb169e17f61/orig",
+      "width": "300px"
+    },
+    {
+      "year": "1147",
+      "title": "Москва",
+      "url": "https://babyben.ru/images/babyben/2017/03/823_b.jpg",
+      "width": "300px"
+    },
+    {
+      "year": "1005",
+      "title": "Казань",
+     "url": "https://turvopros.com/wp-content/uploads/2022/06/mechet-kul-sharif.jpg",
+      "width": "300px"
+    }
+]
+`;
